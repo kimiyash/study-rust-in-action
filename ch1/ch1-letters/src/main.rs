5000c4e56c6a3dfcf72665bbfcc2f9d86bb8ea4c
kimiyash <kimiyash@gmail.com>
2024-09-13 09:52:12 +0900

    tweak

--- ch1/ch1-letters/src/main.rs
+++ ch1/ch1-letters/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let mut letters = vec!["a", "b", "c"];
-    let letters_copy = letters.clone();
-
-    for letter in &letters_copy {
+    let mut letters: Vec<String> = ["a", "b", "c"].iter().map(|&s| s.to_string()).collect();
+    let mut letters_copy = letters.clone();
+    let mut index = 0;
+    for letter in &mut letters_copy {
         println!("{}", letter);
-        letters.push(*letter);
+        letters.push(letter.clone());
+        
+        index += 1;
+        *letter = index.to_string();
     }
 
-    println!("{:?}", letters_copy);    
+    println!("{:?}", letters_copy);
     println!("{:?}", letters);
 }
