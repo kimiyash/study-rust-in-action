140002c3610a7869151cd2a94fd4334e83f9c244
kimiyash <kimiyash@gmail.com>
2024-09-14 09:59:28 +0900

    tweak

--- ch3/ch3-notquite-file-1/src/main.rs
+++ ch3/ch3-notquite-file-1/src/main.rs
@@ -1,46 +1,66 @@
-#![allow(unused_variables)]
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
 
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
-fn open(f: &mut File) -> bool {
-    true
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+
+    fn read(
+        self: &File,
+        save_to: &mut Vec<u8>
+    ) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for readinng"));
+        }
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
+    Ok(f)
 }
 
-fn read(
-    f: &mut File,
-    save_to: &mut Vec<u8>
-) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
+    Ok(f)
 }
 
 fn main() {
-    let mut f2 = File {
-        name: String::from("2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let mut f = File::new("5.txt");
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f2);
-    let f2_length = read(&mut f2, &mut buffer);
-    close(&mut f2);
+    if f.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    f = open(f).unwrap();
+    let f3_length = f.read(&mut buffer).unwrap();
+    f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f2);
-    println!("{} is {} byte long", &f2.name, f2_length);
+    println!("{:?}", f);
+    println!("{} is {} byte long", &f.name, f3_length);
     println!("{}", text);
 }
