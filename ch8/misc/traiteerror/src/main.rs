b914d5189ce755ef027ce26aba495dd343115fcc
kimiyash <kimiyash@gmail.com>
2024-09-18 14:52:21 +0900

    traiteerror

--- ch8/misc/traiteerror/src/main.rs
+++ ch8/misc/traiteerror/src/main.rs
@@ -1,12 +1,32 @@
+use std::error;
+use std::io;
+use std::fmt;
+use std::net;
 use std::fs::File;
-use std::error::Error;
 use std::net::Ipv6Addr;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let _f = File::open("invisible.txt")?;
+#[derive(Debug)]
+enum UpStreamError {
+    IO(io::Error),
+    Parsing(net::AddrParseError),
+}
+
+impl fmt::Display for UpStreamError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl error::Error for UpStreamError {}
+
+fn main() -> Result<(), UpStreamError> {
+    let _f = File::open("invisible.txt")
+        .map_err(UpStreamError::IO)?;
 
     let _localhost = "::1"
-        .parse::<Ipv6Addr>()?;
+        .parse::<Ipv6Addr>()
+        .map_err(UpStreamError::Parsing)?;
+
 
     Ok(())
 }
