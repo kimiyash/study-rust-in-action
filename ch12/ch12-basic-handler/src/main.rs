4e1dd14b1eb718776c5240bc024c3f99c5e517aa
kimiyash <kimiyash@gmail.com>
2024-09-22 09:48:11 +0900

    ch12-basic-handler

--- ch12/ch12-basic-handler/src/main.rs
+++ ch12/ch12-basic-handler/src/main.rs
@@ -1,3 +1,58 @@
+#![cfg(not(windows))]
+
+use std::time::{Duration};
+use std::thread::{sleep};
+use libc::{SIGTERM, SIGUSR1};
+
+static mut SHUT_DONW: bool = false;
+
 fn main() {
-    println!("Hello, world!");
+    register_signal_handlers();
+
+    let delay = Duration::from_secs(1);
+
+    for i in 1_usize.. {
+        println!("{}", i);
+        unsafe {
+            if SHUT_DONW {
+                println!("*");
+                return;
+            }
+        }
+        sleep(delay);
+
+        let signal = if i > 2 {
+            SIGTERM
+        } else {
+            SIGUSR1
+        };
+
+        unsafe {
+            libc::raise(signal);
+        }
+    }
+    unreachable!();
+}
+
+fn register_signal_handlers() {
+    unsafe {
+        libc::signal(SIGTERM, handle_sigtterm as usize);
+        libc::signal(SIGUSR1, handle_sigusr1 as usize);
+    }
+}
+
+#[allow(dead_code)]
+fn handle_sigtterm(_signal: i32) {
+    register_signal_handlers();
+    println!("SIGTTERM");
+
+    unsafe {
+        SHUT_DONW = true;
+    }
+}
+
+#[allow(dead_code)]
+fn handle_sigusr1(_signal: i32) {
+    register_signal_handlers();
+    println!("SIGUSR1")
 }
