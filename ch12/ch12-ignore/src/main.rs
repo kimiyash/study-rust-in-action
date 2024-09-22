e7327904b54f958b0df4f7abe28823f5492167b1
kimiyash <kimiyash@gmail.com>
2024-09-22 10:43:51 +0900

    ch12-ignore

--- ch12/ch12-ignore/src/main.rs
+++ ch12/ch12-ignore/src/main.rs
@@ -1,3 +1,18 @@
+use libc::{signal, raise};
+use libc::{SIG_DFL, SIG_IGN, SIGTERM};
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        signal(SIGTERM, SIG_IGN);
+        raise(SIGTERM);
+    }
+
+    println!("ok");
+
+    unsafe {
+        signal(SIGTERM, SIG_DFL);
+        raise(SIGTERM);
+    }
+
+    println!("not ok")
 }
